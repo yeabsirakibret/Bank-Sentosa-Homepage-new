36c08f2358b1ac3e4c2c138513a18f7b90a17b2f
Yeabsira Kibret <62602453+yeabsirakibret@users.noreply.github.com>
2025-07-01 19:12:12 +0300

    rounded flags

--- src/components/LocaleSwitcherSelect.tsx
+++ src/components/LocaleSwitcherSelect.tsx
@@ -45,7 +45,7 @@ export default function LocaleSwitcherSelect({ children, defaultValue, options }
     <div className="relative inline-block text-left" ref={ref}>
       <div>
         <button type="button" className="inline-flex justify-center w-full rounded-full border border-gray-300 shadow-sm px-4 py-2 bg-gray-100 text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={() => setIsOpen(!isOpen)}>
-          <img src={selected?.icon} alt="" className="w-5 h-5 mr-2" />
+          <img src={selected?.icon} alt="" className="w-5 h-5 mr-2 rounded-full" />
           {selected?.label}
           <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
@@ -58,7 +58,7 @@ export default function LocaleSwitcherSelect({ children, defaultValue, options }
           <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             {options.map(option => (
               <a href="#" key={option.value} className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 hover:text-gray-900" role="menuitem" onClick={() => onSelect(option)}>
-                <img src={option.icon} alt="" className="w-5 h-5 mr-2" />
+                <img src={option.icon} alt="" className="w-5 h-5 mr-2 rounded-full" />
                 {option.label}
               </a>
             ))}
